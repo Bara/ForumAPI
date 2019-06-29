630affd48b7af7c1abe17b5c5a12dc7e39624e4c
Bara <mail@bara.dev>
2019-06-29 12:26:51 +0200

    Update test.sh

--- ci/test.sh
+++ ci/test.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-set -ev
 
-echo -e "Download und extract sourcemod"
+echo "Download und extract sourcemod"
 wget "http://www.sourcemod.net/latest.php?version=$1&os=linux" -O sourcemod.tar.gz
 tar -xzf sourcemod.tar.gz
 
-echo -e "Give compiler rights for compile"
+echo "Give compiler rights for compile"
 chmod +x addons/sourcemod/scripting/spcomp
 
-for file in addons/sourcemod/scripting/*.sp
+for file in addons/sourcemod/scripting/forum*.sp
 do
-  echo -e "\nCompiling $file..." 
+  echo "\nCompiling $file..." 
   addons/sourcemod/scripting/spcomp -E -w234 -O2 -v2 $file
 done
